d9a1cc07b333073b85b254aafaa74963c65c0b94
Rafael Xavier <rxaviermontero@gmail.com>
2019-05-25 21:54:31 -0300

    Significant cleanup, reuse data if available

--- crawlers/el_pais.py
+++ crawlers/el_pais.py
@@ -9,35 +9,57 @@ username = "user"
 password = "pass"
 
 
-article_links = []
-for page in range(1, 11):
+def get_comments():
 
-    info_response = requests.get(info_url + str(page)).text
-    info_soup = BeautifulSoup(info_response, features="html.parser")
+    article_links = []
+    for page in range(1, 11):
+        info_response = requests.get(info_url + str(page)).text
+        info_soup = BeautifulSoup(info_response, features="html.parser")
 
-    page_links = info_soup.find_all("a", {'class': 'page-link', 'href': True})
+        page_links = info_soup.find_all("a", {'class': 'page-link', 'href': True})
 
-    for link in page_links:
-        article_links.append(base_url + link["href"])
+        for link in page_links:
+            article_links.append(base_url + link["href"])
 
-article_links = list(set(article_links))
+    article_links = list(set(article_links))
 
+    try:
+        with open("article_links.txt", "r") as txt:
+            previous_links = txt.read().splitlines()
+    except IOError:
+        previous_links = []
 
-comments = []
-with requests.Session() as session:
-    session.post("https://sso.elpais.com.uy/cas/login", data={"username": username, "password": password})
+    unique_links = list(set(article_links) - set(previous_links))
 
-    for article in article_links:
-        article_response = session.get(article).text
-        article_soup = BeautifulSoup(article_response, features="html.parser")
-        article_id = article_soup.find("meta", {"name": "cXenseParse:recs:articleid"})["content"]
-        comments_url = base_comment_url + article_id + "/comments"
+    with open("article_links.txt", "a+") as txt:
+        for link in unique_links:
+            txt.write("%s\n" % link)
 
-        comments_response = session.get(comments_url).text
-        comments_soup = BeautifulSoup(comments_response, features="html.parser")
-        comments_find = comments_soup.find_all("div", {"class": "comment-text"})
+    article_links.extend(unique_links)
 
-        for comment in comments_find:
-            comments.append(comment.get_text().strip())
+    comments = []
+    with requests.Session() as session:
+        session.post("https://sso.elpais.com.uy/cas/login", data={"username": username, "password": password})
 
+        for article in unique_links:
+            article_response = session.get(article).text
+            article_soup = BeautifulSoup(article_response, features="html.parser")
+            article_id = article_soup.find("meta", {"name": "cXenseParse:recs:articleid"})["content"]
 
+            comments_url = base_comment_url + article_id + "/comments"
+
+            comments_response = session.get(comments_url).text
+            comments_soup = BeautifulSoup(comments_response, features="html.parser")
+            comments_find = comments_soup.find_all("div", {"class": "comment-text"})
+
+            for comment in comments_find:
+                comments.append(comment.get_text().strip())
+
+    with open("comments.txt", "a+") as txt:
+        for comment in comments:
+            txt.write("%s\n" % comment)
+
+    with open("comments.txt", "r") as txt:
+        comments_text = txt.read()
+
+    return article_links, comments_text
