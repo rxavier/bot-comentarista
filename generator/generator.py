8676538357d6aca7f09bb101b2b3c1c4e33a8b05
Rafael Xavier <rxaviermontero@gmail.com>
2019-06-01 01:32:52 -0300

    Move all sentence creation logic outside script. Now only creates model

--- generator/generator.py
+++ generator/generator.py
@@ -3,40 +3,17 @@ import json
 from crawlers import el_pais
 
 
-def get_tweet(sentences=1, new=True, tweet=True):
+def make_model():
 
-    if new is True:
-        el_pais.get_comments()
+    el_pais.get_comments()
 
-        with open("../crawlers/comments.txt", "r") as txt:
-            comments_text = txt.read()
+    with open("../crawlers/comments.txt", "r") as txt:
+        comments_text = txt.read()
 
-        model = markovify.NewlineText(comments_text)
+    model = markovify.NewlineText(comments_text)
 
-        model_json = model.to_json()
-        with open("../generator/model.json", "w") as f:
-            json.dump(model_json, f)
+    model_json = model.to_json()
+    with open("../generator/model.json", "w") as f:
+        json.dump(model_json, f)
 
-    else:
-        with open("../generator/model.json", "r") as f:
-            model_json = json.load(f)
-        model = markovify.NewlineText.from_json(model_json)
-
-    if sentences > 1:
-
-        output = []
-        for i in range(sentences):
-
-            if tweet is True:
-                output.append(model.make_short_sentence(280))
-            else:
-                output.append(model.make_sentence())
-
-    else:
-
-        if tweet is True:
-            output = model.make_short_sentence(280)
-        else:
-            output = model.make_sentence()
-
-    return output
+    return model
