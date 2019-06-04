18181bc4e8abecd17b8ecec040b1e815a7781cbd
Rafael Xavier <rxaviermontero@gmail.com>
2019-06-03 23:18:34 -0300

    Add function for replying to tweets with a certain hashtag

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import tweepy
 import markovify
 import json
+import time
 from generator import generator
 
 consumer_key = "consumer_key"
@@ -8,14 +9,15 @@ consumer_secret = "consumer_secret"
 access_token = "access_token"
 access_token_secret = "access_token_secret"
 
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-def make_tweet(new=False):
+def make_tweet(update_model=False, update_status=True):
 
-    if new is True:
+    if update_model is True:
         model = generator.make_model()
 
     else:
@@ -25,5 +27,31 @@ def make_tweet(new=False):
 
     tweet = model.make_short_sentence(280)
 
-    api.update_status(tweet)
-    print("Tweeted: " + tweet)
+    if update_status is True:
+        api.update_status(tweet)
+        print(f"Tweeted {tweet}")
+
+    return tweet
+
+
+def fulfill_request():
+
+    last_reply_id = None
+    for status in tweepy.Cursor(api.home_timeline).items(50):
+        if status.in_reply_to_status_id is not None:
+            last_reply_id = status.in_reply_to_status_id
+            break
+        else:
+            pass
+
+    if last_reply_id is not None:
+        cursor = tweepy.Cursor(api.search, q=f"@{handle} -filter:retweets + #{hashtag}",
+                               since_id=last_reply_id).items(50)
+    else:
+        cursor = tweepy.Cursor(api.search, q=f"@{handle} -filter:retweets + #{hashtag}").items(50)
+
+    for reply in cursor:
+
+        request_reply = make_tweet(update_model=False, update_status=False)
+        api.update_status(request_reply, in_reply_to_status_id=reply.id, auto_populate_reply_metadata=True)
+        time.sleep(10)
